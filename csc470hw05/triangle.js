012462251769427c5705eaee9feb47bfda4fb719
cedrichansen <ceddy29@yahoo.ca>
2019-11-06 09:33:41 -0500

    fixed the mistake! :) all done

--- csc470hw05/triangle.js
+++ csc470hw05/triangle.js
@@ -563,6 +563,12 @@ function addSquare(xVal, yVal, width) {
         vec3(0,0, -1),
         vec3(0,0, -1),
 
+        vec3(-1, 0, 0),
+        vec3(-1, 0, 0),
+        vec3(-1, 0, 0),
+        vec3(-1, 0, 0),
+        vec3(-1, 0, 0),
+        vec3(-1, 0, 0),
 
         vec3(1,0,0),
         vec3(1,0,0),
@@ -571,12 +577,7 @@ function addSquare(xVal, yVal, width) {
         vec3(1,0,0),
         vec3(1,0,0),
 
-        vec3(-1, 0, 0),
-        vec3(-1, 0, 0),
-        vec3(-1, 0, 0),
-        vec3(-1, 0, 0),
-        vec3(-1, 0, 0),
-        vec3(-1, 0, 0),
+
 
         vec3(0,1,0),
         vec3(0,1,0),
